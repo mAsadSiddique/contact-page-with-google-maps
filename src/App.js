a9b7e693d99368da168cf0095d544b852ec69f0e
mAsadSiddique <asadsiddique.a@gmail.com>
2021-03-12 15:02:55 +0500

    Created google map-react

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React from 'react'
 
 import IntroSection from './components/intro/Intro'
 import ContactSection from './components/contact-section/ContactSection'
+import MapSection from './components/map/Map.jsx'
 import DisclaimerSection from './components/disclaimer/Disclaimer'
 import FooterSection from './components/footer/Footer'
 
 import './App.css'
+
+
+
+const location = {
+  address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  lat: 37.42216,
+  lng: -122.08427,
+} // our location object from earlier
 
 function App() {
   return (
     <div className="App">
       <IntroSection />
       <ContactSection />
+      <MapSection location={location} zoomLevel={17} /> {/* include it here */}
       <DisclaimerSection />
       <FooterSection />
     </div>
